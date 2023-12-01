f5300d8eabff0ba06739296f6daf780990dd6387
Cláudio <csl.claudioleite@gmail.com>
2023-12-01 01:18:50 -0300

    exercicioum

--- lib/modules/forms/screens/formulario.dart
+++ lib/modules/forms/screens/formulario.dart
@@ -11,9 +11,8 @@ class Formulario extends StatefulWidget {
 }
 
 class FormularioState extends State<Formulario> {
-
   String _sexoSelecionado = "M";
-  bool _webSelecionado=false, _mobileSelecionado=false;
+  bool _webSelecionado = false, _mobileSelecionado = false;
   List<String> _listaSelecionados = [];
   double _idade = 25;
 
@@ -36,135 +35,109 @@ class FormularioState extends State<Formulario> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: SingleChildScrollView( // Serve para adicionar Scroll
-          child: Column(
-            children: [
+        body: Padding(
+            padding: const EdgeInsets.all(30),
+            child: SingleChildScrollView(
+                // Serve para adicionar Scroll
+                child: Column(children: [
               TextField(
                 controller: nameController,
                 keyboardType: TextInputType.name,
                 decoration: const InputDecoration(
-                  hintText: "Seu nome",
-                  prefixIcon: Icon(Icons.person)
-                ),
+                    hintText: "Seu nome", prefixIcon: Icon(Icons.person)),
               ),
               const SizedBox(height: 15),
               TextFormField(
-                maxLines: 5,
-                keyboardType: TextInputType.multiline,
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.article),
-                  hintText: "Sobre você",
-                  border: OutlineInputBorder()
-                )
-              ),
+                  maxLines: 5,
+                  keyboardType: TextInputType.multiline,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.article),
+                      hintText: "Sobre você",
+                      border: OutlineInputBorder())),
               const SizedBox(height: 15),
               TextFormField(
-                keyboardType: TextInputType.phone,
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.phone),
-                  hintText: "Telefone",
-                  border: OutlineInputBorder()
-                )
-              ),
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.phone),
+                      hintText: "Telefone",
+                      border: OutlineInputBorder())),
               const SizedBox(height: 15),
               TextFormField(
                 keyboardType: TextInputType.emailAddress,
                 decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.email),
-                  hintText: "E-mail",
-                  border: OutlineInputBorder()
-                ),
+                    prefixIcon: Icon(Icons.email),
+                    hintText: "E-mail",
+                    border: OutlineInputBorder()),
               ),
               const SizedBox(height: 15),
               DropdownButtonFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Cidade",
-                  prefixIcon: Icon(Icons.location_city)
-                ),
-                items: const [
-                  DropdownMenuItem(
-                    value: "Londrina",
-                    child: Text("Londrina-PR")
-                  ),
-                  DropdownMenuItem(
-                    value: "Cambé",
-                    child: Text("Cambé-PR")
-                  ),
-                  DropdownMenuItem(
-                    value: "Rolândia",
-                    child: Text("Rolândia-PR")
-                  )
-                ], 
-                onChanged: (value) => {
-                  print(value)
-                }
-              ),
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Cidade",
+                      prefixIcon: Icon(Icons.location_city)),
+                  items: const [
+                    DropdownMenuItem(
+                        value: "Londrina", child: Text("Londrina-PR")),
+                    DropdownMenuItem(value: "Cambé", child: Text("Cambé-PR")),
+                    DropdownMenuItem(
+                        value: "Rolândia", child: Text("Rolândia-PR"))
+                  ],
+                  onChanged: (value) => {print(value)}),
               const SizedBox(height: 15),
-              Column(
-                children: [
-                  ListTile(
+              Column(children: [
+                ListTile(
                     title: const Text("Masculino"),
                     leading: Radio(
-                      value: "M",
-                      groupValue: _sexoSelecionado,
-                      onChanged: (value) {
-                        setState(() {
-                          _sexoSelecionado = value!;
-                        });
-                      }
-                    )
-                  ),
-                  ListTile(
+                        value: "M",
+                        groupValue: _sexoSelecionado,
+                        onChanged: (value) {
+                          setState(() {
+                            _sexoSelecionado = value!;
+                          });
+                        })),
+                ListTile(
                     title: const Text("Feminino"),
                     leading: Radio(
-                      value: "F",
-                      groupValue: _sexoSelecionado,
-                      onChanged: (value) {
-                        setState(() {
-                          //value = null;
-                          _sexoSelecionado = value ?? "F";
-                        });
-                      }
-                    )
-                  )
-                ]
-              ),
+                        value: "F",
+                        groupValue: _sexoSelecionado,
+                        onChanged: (value) {
+                          setState(() {
+                            //value = null;
+                            _sexoSelecionado = value ?? "F";
+                          });
+                        }))
+              ]),
               const Divider(),
-              Column(
-                children: [
-                  ListTile(
+              Column(children: [
+                ListTile(
                     title: const Text("Desenvolvimento web"),
                     leading: Checkbox(
-                      value: _webSelecionado,
-                      onChanged: (value) {
-                        setState(() {
-                          _webSelecionado = value!;
-                          (_webSelecionado) ? _listaSelecionados.add("web") : 
-                                              _listaSelecionados.remove("web");
-                          print(_listaSelecionados);
-                        });
-                      }
-                    )
+                        value: _webSelecionado,
+                        onChanged: (value) {
+                          setState(() {
+                            _webSelecionado = value!;
+                            (_webSelecionado)
+                                ? _listaSelecionados.add("web")
+                                : _listaSelecionados.remove("web");
+                            print(_listaSelecionados);
+                          });
+                        })),
+                ListTile(
+                  title: const Text("Desenvolvimento mobile"),
+                  leading: Checkbox(
+                    value: _mobileSelecionado,
+                    onChanged: (value) {
+                      setState(() {
+                        _mobileSelecionado = value!;
+                        (_mobileSelecionado)
+                            ? _listaSelecionados.add("mobile")
+                            : _listaSelecionados.remove("mobile");
+                        print(_listaSelecionados);
+                      });
+                    },
                   ),
-                  ListTile(
-                    title: const Text("Desenvolvimento mobile"),
-                    leading: Checkbox(
-                      value: _mobileSelecionado,
-                      onChanged:(value) {
-                        setState(() {
-                          _mobileSelecionado = value!;
-                          (_mobileSelecionado) ? _listaSelecionados.add("mobile") : 
-                                                 _listaSelecionados.remove("mobile");
-                          print(_listaSelecionados);
-                        });
-                      },
-                    ),
-                  )
-                ]
-              ),
+                )
+              ]),
               const Divider(),
               Slider(
                 value: _idade,
@@ -182,16 +155,12 @@ class FormularioState extends State<Formulario> {
               FilledButton(
                 child: const Text("Salvar"),
                 onPressed: () async {
-                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
                   // Definindo uma chave-valor Global
                   await prefs.setString('userName', nameController.text);
                 },
               )
-            ]
-          )
-        )
-      )
-    );
+            ]))));
   }
-
 }
